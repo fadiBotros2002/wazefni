c51e3553ed88fe98e359501005595a5601bfb451
Fadi <fadibotros99@gmail.com>
2024-10-29 00:31:43 +0300

    hello world just for test

--- database/migrations/2024_10_28_193342_create_questions_table.php
+++ database/migrations/2024_10_28_193342_create_questions_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->json('options');
             $table->string('answer');
             $table->timestamps();
+            //////hello world
         });
     }
 
